a34afa6cad42163360cd864b117ad20bd9d244c4
Geoff Pleiss <gpleiss@gmail.com>
2016-11-19 18:17:10 -0500

    Add nTrain option

--- utils/OpCounter.lua
+++ utils/OpCounter.lua
@@ -188,6 +188,7 @@ OpCounter.module_handlers = {
   ['nn.Reshape'] = OpCounter.ops_nothing,
   ['nn.Dropout'] = OpCounter.ops_nothing, -- Is turned off in inference
   ['nn.Concat'] = OpCounter.ops_nothing,
+  ['nn.MulConstant'] = OpCounter.ops_nothing,
 }
 
 return M.OpCounter
