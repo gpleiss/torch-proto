d1779ac0a26976ad0a16ac3c49fddf468c28bca0
Geoff Pleiss <gpleiss@gmail.com>
2017-01-23 07:54:30 -0500

    Update opts

--- opts.lua
+++ opts.lua
@@ -19,7 +19,7 @@ function M.parse(arg)
   cmd:option('-data',  os.getenv('DATA_DIR') or '', 'Path to datasets')
   cmd:option('-save',  os.getenv('SAVE_DIR') or '/tmp', 'Directory in which to save checkpoints/results')
   ------------- General options --------------------
-  cmd:option('-dataset',   'cifar10', 'Options: imagenet | cifar10 | cifar100')
+  cmd:option('-dataset',   '', 'Options: imagenet | cifar10 | cifar100')
   cmd:option('-manualSeed', 0,       'Manually set RNG seed')
   cmd:option('-nGPU',     1,       'Number of GPUs to use by default')
   cmd:option('-backend',   'cudnn',   'Options: cudnn | cunn')
@@ -33,7 +33,7 @@ function M.parse(arg)
   cmd:option('-nTrain',     math.huge,    'Number of training samples')
   cmd:option('-testOnly',      false, 'Run testing only')
   cmd:option('-testOnValid',   false, 'Do final test on validation (rather than test) set')
-  cmd:option('-tenCrop',      'false', 'Ten-crop testing')
+  cmd:option('-tenCrop',      false, 'Ten-crop testing')
   cmd:option('-noAug', false, 'No data augmentation')
   ------------- Checkpointing options ---------------
   cmd:option('-resume',       false,  'Resume from the latest checkpoint in this directory')
@@ -43,59 +43,60 @@ function M.parse(arg)
   cmd:option('-momentum',      0.9,  'momentum')
   cmd:option('-weightDecay',    1e-4,  'weight decay')
   ---------- Model options ----------------------------------
-  cmd:option('-netType',    'resnet', 'Options: resnet | preresnet')
+  cmd:option('-netType',    '', 'Options: resnet | preresnet')
   cmd:option('-depth',      0,     'ResNet depth: 18 | 34 | 50 | 101 | ...', 'number')
-  cmd:option('-width',      1,     'Wide-ResNet width: 1 | 2 | 4', 'number')
+  cmd:option('-width',      0,     'Wide-ResNet width: 1 | 2 | 4', 'number')
   cmd:option('-shortcutType', '',     'Options: A | B | C')
   cmd:option('-transitionType', 'full',     'Options: dense | partial')
   cmd:option('-growthRate', 12,  'How many filters to add each layer')
   cmd:option('-nInitChannels', 16,  'Number of channels to start with initially')
   cmd:option('-dropRate', 0,     'Dropout rate')
   ---------- Model options ----------------------------------
-  cmd:option('-shareGradInput',  'false', 'Share gradInput tensors to reduce memory usage')
-  cmd:option('-optnet',          'false', 'Use optnet to reduce memory usage')
-  cmd:option('-resetClassifier', 'false', 'Reset the fully connected layer for fine-tuning')
+  cmd:option('-shareGradInput',  false, 'Share gradInput tensors to reduce memory usage')
+  cmd:option('-optnet',          false, 'Use optnet to reduce memory usage')
+  cmd:option('-resetClassifier', false, 'Reset the fully connected layer for fine-tuning')
   cmd:option('-pretrained', 'none', 'Pretrained')
   cmd:option('-nClasses',      0,    'Number of classes in the dataset')
   cmd:text()
 
   local opt = cmd:parse(arg or {})
 
-  opt.tenCrop = opt.tenCrop ~= 'false'
-  opt.shareGradInput = opt.shareGradInput ~= 'false'
-  opt.optnet = opt.optnet ~= 'false'
-  opt.resetClassifier = opt.resetClassifier ~= 'false'
   opt.pretrained = (opt.pretrained ~= 'none') and opt.pretrained
 
   if opt.shareGradInput and opt.optnet then
     cmd:error('error: cannot use both -shareGradInput and -optnet')
   end
 
+  if opt.dataset == '' then
+    cmd:error('Must specify dataset')
+  end
+
   -- Model/dataset specific opts --
   local specificOpts = {}
 
   if opt.dataset == 'imagenet' then
+    specificOpts.netType = 'resnet'
+    specificOpts.depth = 18
     specificOpts.shortcutType = 'B'
     specificOpts.nEpochs = 90
     specificOpts.batchSize = 32
     specificOpts.nClasses = 1000
 
   elseif opt.dataset == 'cifar10' then
+    specificOpts.netType = 'densenet'
+    specificOpts.depth = 40
     specificOpts.shortcutType = 'A'
     specificOpts.nEpochs = 300
-    specificOpts.batchSize = (opt.nettype == 'densenet') and 64 or  256
+    specificOpts.batchSize = (opt.netType == '' or opt.netType == 'densenet') and 64 or 256
     specificOpts.nClasses = 10
 
   elseif opt.dataset == 'cifar100' then
+    specificOpts.netType = 'densenet'
+    specificOpts.depth = 40
     specificOpts.shortcutType = 'A'
     specificOpts.nEpochs = 300
-    specificOpts.batchSize = (opt.nettype == 'densenet') and 64 or  256
+    specificOpts.batchSize = (opt.netType == '' or opt.netType == 'densenet') and 64 or 256
     specificOpts.nClasses = 100
-
-  elseif opt.dataset == 'celeba' then
-    specificOpts.nEpochs = 150
-    specificOpts.batchSize = 256
-    specificOpts.nClasses = opt.attr and 40 or 8000
 
   else
     cmd:error('unknown dataset: ' .. opt.dataset)
@@ -123,13 +124,10 @@ function M.parse(arg)
     assert(res, 'Could not open or make save directory ' .. opt.save)
   end
 
-  opt.pretrained = opt.pretrained ~= 'none' and opt.pretrained
-
   opt.modelFilename = paths.concat(opt.save, 'model.t7')
   opt.optimFilename = paths.concat(opt.save, 'optimState.t7')
   opt.loggerFilename = paths.concat(opt.save, 'log.t7')
   opt.latestFilename = paths.concat(opt.save, 'latest.t7')
-  opt.trainScoresFilename = paths.concat(opt.save, 'train-scores.mat')
   opt.testScoresFilename = paths.concat(opt.save, 'test-scores.mat')
 
   opt.datasetDir = paths.concat(opt.data, opt.dataset)
